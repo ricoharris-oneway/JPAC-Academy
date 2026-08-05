6c1be01511d19371bd73fb27f0f0ccd8d3aaa09c
ricoharris-oneway <ricoharris@gmail.com>
2026-08-04 20:25:50 -0400

    Wrap JPAC Academy in C2 auth provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,1 @@
-import React from'react';import ReactDOM from'react-dom/client';import App from'./App';import'./styles/global.css';ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>);
+import React from'react';import ReactDOM from'react-dom/client';import App from'./App';import{AuthProvider}from'./context/AuthContext';import'./styles/global.css';ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><AuthProvider><App/></AuthProvider></React.StrictMode>);
